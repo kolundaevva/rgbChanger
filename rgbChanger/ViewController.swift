e5457f8aabd7c1746764792ef293dddb24f16a52
Vladislav Kolundaev <kolundaevva@gmail.com>
2020-10-19 17:57:06 +0300

    Update all

--- rgbChanger/ViewController.swift
+++ rgbChanger/ViewController.swift
@@ -8,30 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var viewOutler: UIView!
-    @IBOutlet var redController: UISlider!
-    @IBOutlet var greenConroller: UISlider!
-    @IBOutlet var blueController: UISlider!
-    @IBOutlet var redInfrormation: UILabel!
-    @IBOutlet var greenInfromation: UILabel!
+    @IBOutlet var viewInformation: UIView!
+    
+    @IBOutlet var redInformation: UILabel!
+    @IBOutlet var greenInformation: UILabel!
     @IBOutlet var blueInformation: UILabel!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        viewOutler.layer.cornerRadius = 10
+        viewInformation.layer.cornerRadius = 10
+        
+        redInformation.numberOfLines = 1
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        greenSlider.minimumTrackTintColor = .green
     }
     
-    func sliderColor() {
-        redController.minimumTrackTintColor = .red
-        greenConroller.minimumTrackTintColor = .green
-        blueController.minimumTrackTintColor = .blue
+    @IBAction func redSliderAction() {
+        redInformation.text = String(format: "%.2f", redSlider.value)
+        viewInformation.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
-
-    @IBAction func redAction() {
+    @IBAction func greenSliderAction() {
+        greenInformation.text = String(format: "%.2f", greenSlider.value)
+        viewInformation.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
-    @IBAction func greenAction() {
-    }
-    @IBAction func blueAction() {
+    @IBAction func blueSliderAction() {
+        blueInformation.text = String(format: "%.2f", blueSlider.value)
+        viewInformation.backgroundColor = UIColor(displayP3Red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
     
 }
